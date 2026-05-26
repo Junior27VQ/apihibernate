13870f5f880f4f2cc639777de5f72632f41e2db6
Junior27VQ <favq1999@outlook.com>
2026-05-25 22:18:29 -0500

    FAVQ: services created

---
+++ src/main/java/com/krakedev/peliculas/services/ServicioPelicula.java
@@ -0,0 +1,60 @@
+package com.krakedev.peliculas.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.kradev.empleados.entidades.Empleado;
+import com.krakedev.peliculas.entidades.Pelicula;
+import com.krakedev.peliculas.repository.PeliculaRepository;
+
+@Service
+public class ServicioPelicula {
+	private final PeliculaRepository repository;
+
+	public ServicioPelicula(PeliculaRepository repository) {
+		super();
+		this.repository = repository;
+	}
+	
+	public Pelicula crear(Pelicula peli) {
+		return repository.save(peli);
+	};
+	public List<Pelicula> listar(){
+		return repository.findAll();
+	};
+	public Pelicula buscarPorId(Long id) {
+		Optional<Pelicula> peli = repository.findById(id);
+		return peli.orElse(null);
+	};
+	public Pelicula actualizar(Long id, Pelicula peli) {
+		Pelicula encontrado = buscarPorId(id);
+		if(encontrado == null) {
+			return null;
+		}
+		encontrado.setNombre(peli.getNombre());
+		encontrado.setDirector(peli.getDirector());
+		encontrado.setGenero(peli.getGenero());
+		encontrado.setDuracion(peli.getDuracion());
+		encontrado.setDisponible(peli.isDisponible());
+		
+		return repository.save(encontrado);
+	};
+	public boolean eliminar(Long id) {
+		Pelicula peli = buscarPorId(id);
+		if(peli == null) {
+			return false;
+		}
+		repository.deleteById(id);
+		return true;
+	};
+	
+	public List<Pelicula> buscarPorCargo(String genero){
+		return repository.findByGenero(genero);
+	}
+	public List<Pelicula> buscarPorEstado(boolean disponible){
+		return repository.findByDisponible(disponible);
+	}
+
+}
